25b7750f5414d3c82f1f328de39e52dd6e7d44f1
sgmonusg <sagargupta_monu@yahoo.co.in>
2016-09-01 02:11:49 +0530

    replaced with a static page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(window).load(function() {
     //$("#loading").delay(2000).fadeOut(500);
     $("#loading").fadeOut(1100);
     $('body').css({
-        'overflow': 'auto'
-    })
+        'overflowX': 'hidden',
+        'overflowY': 'scroll'
+    });
 });
--- js/transitions.js
+++ js/transitions.js
@@ -4,63 +4,66 @@ $(document).ready(function() {
     //     $('#loading').fadeOut('slow', function() { $(this).remove(); });
     // });
     /* Every time the window is scrolled ... */
-    $(window).scroll(function() {
-        var top = $(this).scrollTop();
-        /* Check the location of each desired element */
-        $('.left').each(function(i) {
-            if ($(this).offset().top < top + 600) {
-                $(this).css({
-                    'opacity': '1',
-                    'transform': 'translateX(200px)'
-                });
-            }
+    if (window.innerWidth >= 1200) {
+        $(window).scroll(function() {
+            var top = $(this).scrollTop();
+            /* Check the location of each desired element */
+            $('.left').each(function(i) {
+                if ($(this).offset().top < top + 600) {
+                    $(this).css({
+                        'opacity': '1',
+                        'transform': 'translateX(200px)'
+                    });
+                }
+
+            });
+            $('.center').each(function(i) {
+                if ($(this).offset().top < top + 600) {
+                    $(this).css({
+                        'opacity': '1'
+                    });
+                }
+
+            });
+            $('.right').each(function(i) {
+                if ($(this).offset().top < top + 600) {
+                    $(this).css({
+                        'opacity': '1',
+                        'transform': 'translateX(0px)'
+                    });
+                }
+
+            });
+            $('.timeline-image').each(function(i) {
+                if ($(this).offset().top < top + 600) {
+                    $(this).css({
+                        'animation': 'cd-bounce 0.6s',
+                        'opacity': '1'
+                    });
+                }
+
+            });
+            $('.timeline-left').each(function(i) {
+                if ($(this).offset().top < top + 600) {
+                    $(this).css({
+                        'opacity': '1',
+                        'transform': 'translateX(0px)'
+
+                    });
+                }
+
+            });
+            $('.timeline-right').each(function(i) {
+                if ($(this).offset().top < top + 600) {
+                    $(this).css({
+                        'opacity': '1',
+                        'transform': 'translateX(0px)'
+                    });
+                }
+
+            });
 
         });
-        $('.center').each(function(i) {
-            if ($(this).offset().top < top + 600) {
-                $(this).css({
-                    'opacity': '1'
-                });
-            }
 
-        });
-        $('.right').each(function(i) {
-            if ($(this).offset().top < top + 600) {
-                $(this).css({
-                    'opacity': '1',
-                    'transform': 'translateX(00px)'
-                });
-            }
-
-        });
-        $('.timeline-image').each(function(i) {
-            if ($(this).offset().top < top + 600) {
-                $(this).css({
-                    'animation': 'cd-bounce 0.6s',
-                    'opacity': '1'
-                });
-            }
-
-        });
-        $('.timeline-left').each(function(i) {
-            if ($(this).offset().top < top + 600) {
-                $(this).css({
-                    'opacity': '1',
-                    'transform': 'translate(0px)'
-                });
-            }
-
-        });
-        $('.timeline-right').each(function(i) {
-            if ($(this).offset().top < top + 600) {
-                $(this).css({
-                    'opacity': '1',
-                    'transform': 'translate(0px)'
-                });
-            }
-
-        });
-
-    });
-
+    }
 });
